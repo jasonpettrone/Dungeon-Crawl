2443b623dc76d919374d167233180a8ff05cc23a
SolanoMauri <43177174+SolanoMauri@users.noreply.github.com>
2018-10-07 00:29:09 -0400

    Implemented Interaction between Idle and Active Animation
    Fixed Issue where Idle and Active animations were not interacting with each other properly. When moving left and stopped, character would turn and face down. Now it stays facing the direction you are walking.

--- Dungeon Crawl/Assets/Scripts/PlayerController.cs
+++ Dungeon Crawl/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour {
 
     public float moveSpeed;
     private Animator anim;
+    private bool playerActive;
+    private Vector2 lastMove;
 
 	// Use this for initialization
 	void Start () {
@@ -14,18 +16,26 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        
+        playerActive = false;
         if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
         {
             transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f));
+            playerActive = true;
+            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
         }
 
         if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
         {
             transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime));
+            playerActive = true;
+            lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
         }
 
         anim.SetFloat("moveX", Input.GetAxisRaw("Horizontal"));
         anim.SetFloat("moveY", Input.GetAxisRaw("Vertical"));
+        anim.SetBool("playerActive", playerActive);
+        anim.SetFloat("lastmoveX", lastMove.x);
+        anim.SetFloat("lastmoveY", lastMove.y);
+
     }
 }
