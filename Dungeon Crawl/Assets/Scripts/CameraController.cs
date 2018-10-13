5a372c4ed60e9bcbc40c82aaf100aebd17e52617
Jason Pettrone <jasonpettrone@gmail.com>
2018-10-12 20:20:54 -0400

    Updated CameraController script to follow the Player with the Main Camera.

--- Dungeon Crawl/Assets/Scripts/CameraController.cs
+++ Dungeon Crawl/Assets/Scripts/CameraController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
+    public GameObject followTarget;
+    private Vector3 targetPos;
+    public float moveSpeed;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +15,7 @@ public class CameraController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        targetPos = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, targetPos, moveSpeed * Time.deltaTime);
 	}
 }
