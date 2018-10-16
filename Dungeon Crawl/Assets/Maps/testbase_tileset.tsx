196120ad4e03a42d1ea8afc193ee7a570bc57eae
SolanoMauri <43177174+SolanoMauri@users.noreply.github.com>
2018-10-16 15:53:38 -0400

    deleted some unneeded maps I created

--- Dungeon Crawl/Assets/Maps/testbase_tileset.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.0" name="testbase_tileset" tilewidth="16" tileheight="16" spacing="1" tilecount="1767" columns="57">
- <image source="../Art/roguelikeSheet_transparent.png" width="968" height="526"/>
- <tile id="60">
-  <objectgroup draworder="index">
-   <object id="5" x="-0.125" y="0.125" width="16.125" height="16"/>
-  </objectgroup>
- </tile>
-</tileset>
