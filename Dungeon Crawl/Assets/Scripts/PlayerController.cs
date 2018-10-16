b5f1a35ba5cf5da0acd6d892f0f7fb443afb5f61
SolanoMauri <43177174+SolanoMauri@users.noreply.github.com>
2018-10-16 15:17:08 -0400

    implemented Proper Charater Collision
    #37  The character now collides with map objects properly. We just have to create collision boxes on new maps and run the game.

--- Dungeon Crawl/Assets/Scripts/PlayerController.cs
+++ Dungeon Crawl/Assets/Scripts/PlayerController.cs
@@ -6,12 +6,15 @@ public class PlayerController : MonoBehaviour {
 
     public float moveSpeed;
     private Animator anim;
+    private Rigidbody2D myRigidbody;
+
     private bool playerActive;
     private Vector2 lastMove;
 
 	// Use this for initialization
 	void Start () {
         anim = GetComponent<Animator>();
+        myRigidbody = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
@@ -19,18 +22,27 @@ public class PlayerController : MonoBehaviour {
         playerActive = false;
         if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
         {
-            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f));
+            //transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f));
+            myRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, myRigidbody.velocity.y);
             playerActive = true;
             lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
         }
 
         if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
         {
-            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime));
+            //transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime));
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
             playerActive = true;
             lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
         }
-
+        if(Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
+        {
+            myRigidbody.velocity = new Vector2(0f, myRigidbody.velocity.y);
+        }
+        if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
+        {
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, 0f);
+        }
         anim.SetFloat("moveX", Input.GetAxisRaw("Horizontal"));
         anim.SetFloat("moveY", Input.GetAxisRaw("Vertical"));
         anim.SetBool("playerActive", playerActive);
