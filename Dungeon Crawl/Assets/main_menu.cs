ffe1a378ef72ab25f22e66c80bc4d8fc9b8ec06c
Jason Pettrone <jasonpettrone@gmail.com>
2018-10-24 14:37:32 -0400

    Modified main menu scene to load up "test_scene_ instead of "main".

--- Dungeon Crawl/Assets/main_menu.cs
+++ Dungeon Crawl/Assets/main_menu.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class main_menu : MonoBehaviour {
 
     public void PlayGame(){
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("test_scene");
     }
 
     public void QuitGame(){
