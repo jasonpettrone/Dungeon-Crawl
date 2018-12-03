d276d0c8bdfd5d1bab52de3954d3523c7bec2367
SolanoMauri <43177174+SolanoMauri@users.noreply.github.com>
2018-12-03 15:34:51 -0500

    Create a "HurtEnemy" script that includes a "bleed" animation #81

--- Dungeon Crawl/Assets/Scripts/HurtEnemy.cs
+++ Dungeon Crawl/Assets/Scripts/HurtEnemy.cs
@@ -2,17 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HurtEnemy : MonoBehaviour {
+public class HurtEnemy : MonoBehaviour
+{
+    public int damageToGive;
+    public GameObject damageBurst;
+    public Transform hitPoint;
+    public GameObject damageNumber;
+    // Use this for initialization
+    void Start()
+    {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -20,8 +26,12 @@ public class HurtEnemy : MonoBehaviour {
         {
 
             //other.gameObject.SetActive(false);
-            Destroy(other.gameObject);
-            
+            //Destroy(other.gameObject);
+            other.gameObject.GetComponent<EnemyHealthManager>().HurtEnemy(damageToGive);
+            Instantiate(damageBurst, transform.position, hitPoint.rotation);
+            var clone = (GameObject) Instantiate(damageNumber, hitPoint.position, Quaternion.Euler (Vector3.zero));
+            clone.GetComponent<FloatingNumbers>().damageNumber = damageToGive;
+
         }
     }
 }
