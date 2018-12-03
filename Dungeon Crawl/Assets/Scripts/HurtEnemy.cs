00b7685d10f94639f6ae4341bfc4086eb01227ec
SolanoMauri <43177174+SolanoMauri@users.noreply.github.com>
2018-12-03 11:03:53 -0500

    #86 Attach damage textbox animation to attack animation
    Edit "HurtEnemy" Script to create a "damageNumber" variable. Attach the complete textbox animation to damageNumber object in "HurtEnemy" script so that numbers appear when you deal dammage to enemies.

--- Dungeon Crawl/Assets/Scripts/HurtEnemy.cs
+++ Dungeon Crawl/Assets/Scripts/HurtEnemy.cs
@@ -7,7 +7,7 @@ public class HurtEnemy : MonoBehaviour
     public int damageToGive;
     public GameObject damageBurst;
     public Transform hitPoint;
-
+    public GameObject damageNumber;
     // Use this for initialization
     void Start()
     {
@@ -29,6 +29,8 @@ public class HurtEnemy : MonoBehaviour
             //Destroy(other.gameObject);
             other.gameObject.GetComponent<EnemyHealthManager>().HurtEnemy(damageToGive);
             Instantiate(damageBurst, transform.position, hitPoint.rotation);
+            var clone = (GameObject) Instantiate(damageNumber, hitPoint.position, Quaternion.Euler (Vector3.zero));
+            clone.GetComponent<FloatingNumbers>().damageNumber = damageToGive;
 
         }
     }
