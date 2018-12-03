1c00abb3efcd3459e3b6a050bbfaeedf51fa47b8
SolanoMauri <43177174+SolanoMauri@users.noreply.github.com>
2018-12-02 21:09:47 -0500

    enemyHealthManager Script
    Give enemies a health system

---
+++ Dungeon Crawl/Assets/Scripts/EnemyHealthManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthManager : MonoBehaviour {
+
+    public int enemyMaxHealth;
+    public int enemyCurrentHealth;
+
+    // Use this for initialization
+    void Start()
+    {
+        enemyCurrentHealth = enemyMaxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemyCurrentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void HurtEnemy(int damageToGive)
+    {
+        enemyCurrentHealth -= damageToGive;
+    }
+
+    public void SetMaxHealth()
+    {
+        enemyCurrentHealth = enemyMaxHealth;
+    }
+}
